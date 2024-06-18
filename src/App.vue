3b08f25db2246a3cb6b2d5e8d9fecca2999f3c00
sm.haung <sm.huang@trip.com>
2024-06-18 13:01:23 +0800

    618

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="欢迎来到菜鸟教程！" />
 </template>
 
 <script>
